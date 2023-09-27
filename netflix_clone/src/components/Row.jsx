2d00ac1add324a0bd554457b28d40ed067277d66
JosephMumo <josephmumo25@gmail.com>
2023-09-27 15:42:22 +0300

    row component opacity adjustment

--- netflix_clone/src/components/Row.jsx
+++ netflix_clone/src/components/Row.jsx
@@ -17,6 +17,9 @@ function Row({title, UrlFetch}) {
             {movies.map((item, id) => (
                 <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2" key={id}>
                     <img className="w-full h-auto" src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
+                    <div className="text-white absolute top-0 left-0 w-full h-full hover:bg-black/80 hover:opacity-100 opacity-0 grid place-items-center">
+                        <p>{item?.title}</p>
+                    </div>
                 </div>
             ))}
         </div>
