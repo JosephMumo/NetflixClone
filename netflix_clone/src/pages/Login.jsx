0ad898fbe39785b3df82522a750816af6401d36f
JosephMumo <josephmumo25@gmail.com>
2023-10-03 16:59:21 +0300

    updated the bg-on both pages

--- netflix_clone/src/pages/Login.jsx
+++ netflix_clone/src/pages/Login.jsx
@@ -7,10 +7,11 @@ function Login() {
   }
   return (
     <div className='text-white grid place-items-center align-middle h-screen bg-gradient-to-r from-black' style={styles}>
+      <div className='w-full h-screen bg-black/80 absolute'></div>
       <form className='flex flex-col w-1/2 space-y-5 items-center z-10 text-center'>
         <h1 className='text-white text-4xl font-bold'>LOGIN</h1>
-        <input className='bg-inherit text-white w-full  border-b border-white' placeholder='Name' id='name' name='fname'></input>
-        <input className='bg-inherit text-white w-full border-b border-white' placeholder='Email' id='email' name='email'></input>
+        <input className='bg-inherit text-white w-full md:w-1/2  border-b border-white' placeholder='Name' id='name' name='fname'></input>
+        <input className='bg-inherit text-white w-full md:w-1/2 border-b border-white' placeholder='Email' id='email' name='email'></input>
         <button type="submit" className='bg-primary hover:bg-red-600 w-24 p-2 rounded-sm'>Login</button>
       </form>
     </div>
--- netflix_clone/src/pages/Signup.jsx
+++ netflix_clone/src/pages/Signup.jsx
@@ -8,10 +8,11 @@ function Signup() {
   }
   return (
     <div className='text-white grid place-items-center align-middle h-screen' style={styles}>
-      <form className='flex flex-col w-1/2 space-y-5 items-center'>
-      <h1 className='text-white text-4xl font-bold'>SIGNUP</h1>
-        <input className='bg-black bg-inherit w-full  border-b border-white' placeholder='Name' id='name' name='fname'></input>
-        <input className='bg-black bg-inherit w-full border-b border-white' placeholder='Email' id='email' name='email'></input>
+      <div className='w-full h-screen bg-black/80 absolute'></div>
+      <form className='flex flex-col w-1/2 space-y-5 items-center z-10'>
+        <h1 className='text-white text-4xl font-bold'>SIGNUP</h1>
+        <input className='bg-black bg-inherit w-full md:w-1/2  border-b border-white' placeholder='Name' id='name' name='fname'></input>
+        <input className='bg-black bg-inherit w-full md:w-1/2 border-b border-white' placeholder='Email' id='email' name='email'></input>
         <button type="submit" className='bg-primary hover:bg-red-600 w-24 p-2 rounded-sm'>Register</button>
       </form>
     </div>
