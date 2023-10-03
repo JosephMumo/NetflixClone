d08996d85650129189ae97d1cc9d4913946a6d10
JosephMumo <josephmumo25@gmail.com>
2023-10-03 18:31:15 +0300

    tweaking on the designs

--- netflix_clone/src/pages/Login.jsx
+++ netflix_clone/src/pages/Login.jsx
@@ -6,16 +6,19 @@ function Login() {
     backgroundImage: `url(${logo})`
   }
   return (
-    <div className='text-white grid place-items-center align-middle h-screen bg-gradient-to-r from-black' style={styles}>
-      <div className='w-full h-screen bg-black/80 absolute'></div>
-      <form className='flex flex-col w-1/2 space-y-5 items-center z-10 text-center'>
-        <h1 className='text-white text-4xl font-bold'>LOGIN</h1>
-        <input className='bg-inherit text-white w-full md:w-1/2  border-b border-white' placeholder='Name' id='name' name='fname'></input>
-        <input className='bg-inherit text-white w-full md:w-1/2 border-b border-white' placeholder='Email' id='email' name='email'></input>
-        <button type="submit" className='bg-primary hover:bg-red-600 w-24 p-2 rounded-sm'>Login</button>
-      </form>
+    <div className='text-gray-500 grid place-items-center align-middle h-screen' style={styles}>
+      <div className='w-full h-screen bg-black/60 absolute'></div>
+      <div className='w-{80%} h-{60%} md:w-1/3 md:h-2/3 px-8 py-24 bg-black/80 absolute rounded'>
+        <form className='flex flex-col space-y-5 z-10'>
+          <h1 className='text-2xl font-bold'>LOGIN</h1>
+          <input className='bg-gray-800 rounded px-1 h-9 text-white w-full  border-none' placeholder='Name' id='name' name='fname'></input>
+          <input className='bg-gray-800 rounded px-1 h-9 text-white  border-none' placeholder='Email' id='email' name='email'></input>
+          <br />
+          <button type="submit" className='bg-primary hover:bg-red-600 text-white  p-2 rounded-sm'>Login</button>
+        </form>
+      </div>
     </div>
   )
 }
 
-export default Login
+export default Login 
--- netflix_clone/src/pages/Signup.jsx
+++ netflix_clone/src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../images/Net.jpg'
+import { Link } from 'react-router-dom'
 
   
 function Signup() {
@@ -7,14 +8,30 @@ function Signup() {
     backgroundImage: `url(${logo})`,
   }
   return (
-    <div className='text-white grid place-items-center align-middle h-screen' style={styles}>
-      <div className='w-full h-screen bg-black/80 absolute'></div>
-      <form className='flex flex-col w-1/2 space-y-5 items-center z-10'>
-        <h1 className='text-white text-4xl font-bold'>SIGNUP</h1>
-        <input className='bg-black bg-inherit w-full md:w-1/2  border-b border-white' placeholder='Name' id='name' name='fname'></input>
-        <input className='bg-black bg-inherit w-full md:w-1/2 border-b border-white' placeholder='Email' id='email' name='email'></input>
-        <button type="submit" className='bg-primary hover:bg-red-600 w-24 p-2 rounded-sm'>Register</button>
-      </form>
+    <div className='text-gray-500 grid place-items-center align-middle h-screen' style={styles}>
+      <div className='w-full h-screen bg-black/60 absolute'></div>
+      <div className='w-{80%} h-{60%} md:w-1/3 md:h-2/3 px-8 py-24 bg-black/80 absolute rounded'>
+        <form className='flex flex-col space-y-5 z-10'>
+          <h1 className='text-2xl font-bold'>SIGNUP</h1>
+          <input className='bg-gray-800 rounded px-1 h-9 border-none' placeholder='name' id='name' name='fname'></input>
+          <input className='bg-gray-800 rounded px-1 h-9 border-none' placeholder='email' id='email' name='email'></input>
+          <br />
+          <button type="submit" className='bg-primary hover:bg-red-600 text-white p-2 rounded-sm'>Register</button>
+          <div className='flex flex-row justify-between'>
+            <section className='flex flex-row space-x-3'>
+              <input type='checkbox' id='checkbox' name='checkbox'></input>
+              <label htmlFor='checkbox'>Remember me?</label>
+            </section>
+            <p>Need help?</p>
+          </div>
+          <br />
+          <p>Already Subscribed to Netflix?
+            <Link to='/login'>
+              <button className='text-white'>Login</button>
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   )
 }
