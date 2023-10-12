6019419f40a2bdd4423508c01b8f9caa91b159c2
JosephMumo <josephmumo25@gmail.com>
2023-10-12 18:32:44 +0300

    cleared some bugs on the account page

--- netflix_clone/src/components/SavedShows.jsx
+++ netflix_clone/src/components/SavedShows.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { CiCircleChevLeft, CiCircleChevRight } from 'react-icons/ci'
+import { AiOutlineClose } from 'react-icons/ai'
 import { UserAuth } from '../context/AuthContext'
 import { db } from '../firebase'
 import { updateDoc, onSnapshot, doc } from 'firebase/firestore'
@@ -18,28 +19,60 @@ function SavedShows() {
         slider.scrollLeft = slider.scrollLeft + 500
     }
     useEffect(() => {
-        onSnapshot(doc(db, 'users', `${user?.email}`, (doc) => {
-            setMovies(doc.data()?.SavedShows)
-        }))
+        onSnapshot(doc(db, 'users', `${user?.email}`), (doc) => {
+            setMovies(doc.data()?.savedShows)
+        })
     },[user?.email])
+    const movieRef = doc(db, 'users', `${user?.email}`)
+    const deleteShow = async (passedID) => {
+        try {
+          const result = movies.filter((item) => item.id !== passedID)
+          await updateDoc(movieRef, {
+              savedShows: result
+          })
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
-    <div className="text-white font-bold md:text-xl p-4 w-full">
-        <h3 className="text-white font-bold">My List</h3>
-        <div className="relative flex items-center group">
-            <CiCircleChevLeft size={40} onClick={slideLeft} className="hidden group-hover:block z-13 absolute left-1 cursor-pointer" />
-            <div id={"slider"} className="w-full h-full overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar-hide">
-                {movies.map((item, id) => (
-                    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-                    <img className="w-full h-auto" src={`https://image.tmdb.org/t/p/w500/${item?.img}`} alt={item?.title} />
-                    <div className="text-white absolute top-0 left-0 w-full h-full hover:bg-black/80 hover:opacity-100 opacity-0 grid place-items-center">
-                        <p className="white-space-normal text-xs md:text-sm">{item?.title}</p>
-                    </div>
-                </div>
-                ))}
+    <>
+      <h2 className='text-white font-bold md:text-xl p-4'>My Shows</h2>
+      <div className='relative flex items-center group'>
+        <CiCircleChevLeft
+          onClick={slideLeft}
+          className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
+        <div
+          id={'slider'}
+          className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'
+        >
+          {movies.map((item) => (
+            <div
+              key={item.id}
+              className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
+            >
+              <img
+                className='w-full h-auto block'
+                src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
+                alt={item?.title}
+              />
+              <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
+                  {item?.title}
+                </p>
+                <p onClick={()=> deleteShow(item.id)} className='absolute text-gray-300 top-4 right-4'><AiOutlineClose /></p>
+              </div>
             </div>
-            <CiCircleChevRight size={40} onClick={slideRight} className="hidden group-hover:block z-10 absolute right-1 cursor-pointer" />
+          ))}
         </div>
-    </div>
+        <CiCircleChevRight
+          onClick={slideRight}
+          className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
+      </div>
+    </>
   )
 }
 
